452ad16654d91564fb1dc0bb50cb1d02f0798a57
Kishorgujar <91127600+Kishorgujar@users.noreply.github.com>
2024-10-10 23:20:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,20 @@
-provider "aws" {
-  region = "ap-south-1"  # Change to your desired region
-}
-
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "mybucketaccopa"
-  tags = {
-    Name  = "Department"
-    Value = "ACC"
+terraform {
+  backend "s3" {
+    bucket         = "mybackbucket11"  # Replace with your actual S3 bucket name
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
   }
 }
-
+ 
+provider "aws" {
+  region = "ap-south-1"
+}
+ 
+resource "aws_s3_bucket" "example" {
+  bucket = "mybucketaccopa"
+ 
+  tags = {
+    Name        = "Department"
+    Environment = "DevOps"
+  }
+}
